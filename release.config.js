f6a110a0e3dfa7dfab226392fb7714aae4f34d0b
Mikheil Khmaladze <khmaladzemikheili7@gmail.com>
2025-10-25 20:53:52 +0400

    Fix: release

--- release.config.js
+++ release.config.js
@@ -4,14 +4,15 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github"
-      // {
-      //   assets: [
-      //     { path: "build.zip", label: "Build" },
-      //     { path: "coverage.zip", label: "Coverage" }
-      //   ]
-      // }
-    ]
+    "@semantic-release/github"
+    // [
+    //   "@semantic-release/github"
+    //   {
+    //     assets: [
+    //       { path: "build.zip", label: "Build" },
+    //       { path: "coverage.zip", label: "Coverage" }
+    //     ]
+    //   }
+    // ]
   ]
 }
