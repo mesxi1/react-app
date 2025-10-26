16aff5deb5af69eacc8b4aa8e9b7da8bb9d181d6
Mikheil Khmaladze <khmaladzemikheili7@gmail.com>
2025-10-26 21:18:36 +0400

    release 30

--- release.config.js
+++ release.config.js
@@ -1,18 +1,18 @@
-module.exports = {
-  branches: "workflow",
-  repositoryUrl: "https://github.com/mesxi1/react-app",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
-    // [
-    //   "@semantic-release/github"
-    //   // {
-    //   //   assets: [
-    //   //     { path: "build.zip", label: "Build" },
-    //   //     { path: "coverage.zip", label: "Coverage" }
-    //   //   ]
-    //   // }
-    // ]
-  ]
-}
+// module.exports = {
+//   branches: "workflow",
+//   repositoryUrl: "https://github.com/mesxi1/react-app",
+//   plugins: [
+//     "@semantic-release/commit-analyzer",
+//     "@semantic-release/release-notes-generator",
+//     "@semantic-release/github"
+//     // [
+//     //   "@semantic-release/github"
+//     //   // {
+//     //   //   assets: [
+//     //   //     { path: "build.zip", label: "Build" },
+//     //   //     { path: "coverage.zip", label: "Coverage" }
+//     //   //   ]
+//     //   // }
+//     // ]
+//   ]
+// }
